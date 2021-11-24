9b134ba40aa8f67dce0b7898d15ce9434c888689
Petia Latinova <petia@hack.bg>
2021-11-24 11:23:21 +0200

    test: price consumer contract integration test

---
+++ packages/hardhat/test/integration/PriceConsumer.test.ts
@@ -0,0 +1,23 @@
+import { ethers, deployments, network } from 'hardhat';
+import { expect } from 'chai';
+import skip from 'mocha-skip-if';
+import { developmentChains } from '../../helper-hardhat-config';
+import { PriceConsumer } from 'types/typechain';
+
+skip.if(developmentChains.includes(network.name)).
+  describe('PriceConsumer Integration Tests', () => {
+    let priceConsumer: PriceConsumer;
+
+    beforeEach(async () => {
+      const PriceConsumer = await deployments.get('PriceConsumer');
+      priceConsumer = await ethers.getContractAt('PriceConsumer', PriceConsumer.address) as unknown as PriceConsumer;
+    });
+
+    it('should successfully make request and get a result', async () => {
+      const ethAddress = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
+      const usdAddress = '0x0000000000000000000000000000000000000348';
+
+      let result = await priceConsumer.getPrice(ethAddress, usdAddress);
+      expect(result).to.be.gt(0);
+    });
+  });
