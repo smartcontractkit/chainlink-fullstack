9fbe884a9c8aaf53f4d65d79e3eb7c784420751a
imollov <ivomollov@gmail.com>
2022-01-05 14:31:50 +0200

    chore: upgrade dependencies

--- packages/frontend/conf/config.ts
+++ packages/frontend/conf/config.ts
@@ -1,4 +1,11 @@
-import { ChainId, Config, MULTICALL_ADDRESSES } from '@usedapp/core'
+import {
+  ChainId,
+  Config,
+  Rinkeby,
+  Kovan,
+  Localhost,
+  Hardhat,
+} from '@usedapp/core'
 import LocalhostPriceConsumer from '../artifacts/contracts/localhost/PriceConsumer.json'
 import KovanPriceConsumer from '../artifacts/contracts/kovan/PriceConsumer.json'
 import LocalhostPriceConsumerV3 from '../artifacts/contracts/localhost/PriceConsumerV3.json'
@@ -24,14 +31,8 @@ const config: Config = {
     [ChainId.Kovan]: `https://kovan.infura.io/v3/${INFURA_KEY}`,
     [ChainId.Mainnet]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
   },
-  supportedChains: [
-    ChainId.Rinkeby,
-    ChainId.Kovan,
-    ChainId.Hardhat,
-    ChainId.Localhost,
-  ],
+  networks: [Rinkeby, Kovan, Hardhat, Localhost],
   multicallAddresses: {
-    ...MULTICALL_ADDRESSES,
     [ChainId.Hardhat]: MulticallContract.address,
     [ChainId.Localhost]: MulticallContract.address,
   },
